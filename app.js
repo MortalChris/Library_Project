159521c240480e6d413d968dce0328a19db5c676
Christian Valeri <mortlaxris@outlook.com>
2023-02-13 15:24:53 -0600

    Pushes info to array and display on screen

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ let haveReadEl = document.getElementById("haveRead").value;
 let libraryList = document.getElementById("libraryList");
 let submit = document.getElementById("submit");
 // document.getElementById("bookSubmit").reset;
-
 let myLibrary = [];
 let newBook = [];
 // let newDiv
-
 
 function Book(author, title, pageCount, haveRead) {
     this.author = author,
@@ -18,27 +16,45 @@ function Book(author, title, pageCount, haveRead) {
     this.haveRead = haveRead
 };
 
-Book.prototype.library = function(){
-    // newDiv = document.createElement("div");
-    newBook.push(this.author, this.title, this.pageCount, this.haveRead);
-    let newlibrary = myLibrary.concat([newBook]);
-// var arr = [0];
-// var elem = [1, 2, 3];
-// var newArr = arr.concat([elem]);
-// console.log(newArr); // => [0,[1,2,3]]
+
+Book.prototype.info = function(){
+    return this.title + " by " + this.author + ", " + this.pageCount + ", " + this.haveRead
 };
+Book.prototype.library = function(){
+    myLibrary.push([this.author, this.title, this.pageCount, this.haveRead]);
+    // newDiv = document.createElement("div");
+    // newBook.push(this.author, this.title, this.pageCount, this.haveRead);
+    // let newlibrary = myLibrary.concat([newBook]);
+};
+
 
 function addBookToLibrary() {
   // do stuff here
     titleEl = new Book(authorEl, titleEl, pageCountEl, haveReadEl);
+    const newExample = new Book ("2hi", "2Title", "2Page Count", "2Have Read");
 };
+
 
 function displayLibrary(){
     libraryList.textContent = myLibrary;
 };
 
 
+
+const theExample = new Book ("hi", "Title", "Page Count", "Have Read");
+console.log(theExample.info())
+theExample.library();
+newExample.library();
+
+console.log(myLibrary);
 addBookToLibrary();
 displayLibrary();
-console.log(titleEl.author);
-console.log(newBook);
+// console.log(titleEl.author);
+// console.log(newBook);
+
+
+
+// var arr = [0];
+// var elem = [1, 2, 3];
+// var newArr = arr.concat([elem]);
+// console.log(newArr); // => [0,[1,2,3]]
